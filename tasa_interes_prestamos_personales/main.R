866a726b2b5ea43e5e00d1d0ccbe59bbef39f09c
pdelboca <patriciodelboca@gmail.com>
2019-02-05 17:46:02 -0300

    Finalizada tasa interes prestamos personales

--- tasa_interes_prestamos_personales/main.R
+++ tasa_interes_prestamos_personales/main.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(ggplot2)
 library(gganimate)
 
-tasa <- read_csv("./data/tasa_interes_prestamos_personales.csv") %>% 
+tasa <- read_csv("tasa_interes_prestamos_personales/data/tasa_interes_prestamos_personales.csv") %>% 
   mutate(key = "BCRA", #required for tsibble
          fecha = dmy(Fecha),
          valor = Valor / 100) %>% 
@@ -19,8 +19,10 @@ ggplot(tasa) +
   transition_reveal(year_week) +
   labs(title="Evolucion Tasa de Interes Prestamos Personales",
        subtitle="Según datos BCRA.",
+       caption = "Elaborado por @pdelboca en base a datos del BCRA." ,
        x = "Fecha", y = "% Tasa Interes") +
   scale_y_continuous(breaks=seq(0, 0.7, 0.1), labels = scales::percent, minor_breaks = NULL, limits = c(0,0.7)) +
   scale_x_date(breaks = "1 year", date_labels ="%Y", minor_breaks = NULL) +
   theme_minimal()
 
+anim_save("tasa_interes_prestamos_personales/tasa_de_interes_prestamos_personales.gif")
